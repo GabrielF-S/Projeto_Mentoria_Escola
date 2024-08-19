0480ac828336e93d0ad2eb8ca8237d7bfe7edaaa
GabrielF-S <gabrielboni2008@hotmail.com>
2024-08-19 17:58:44 -0300

    Criado metodo put no Openfeig para passar a aprovação para API da escola

--- notas-sender/src/main/java/br/com/mentoria/ibm/notas_sender/feignCliente/UserFeig.java
+++ notas-sender/src/main/java/br/com/mentoria/ibm/notas_sender/feignCliente/UserFeig.java
@@ -5,12 +5,17 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name="projeto-escola", url = "http://localhost:8080")
+@FeignClient(name="projeto-escola", url = "http://localhost:8080/alunos")
 public interface UserFeig {
 
-    @GetMapping(value = "alunos/{id}")
+    @GetMapping(value = "/{id}")
     ResponseEntity<Aluno> findById(@PathVariable int id);
+
+    @PutMapping(value = "/aprovacao")
+    ResponseEntity<?> updateAluno(@RequestBody Aluno aluno);
 
 
 }
