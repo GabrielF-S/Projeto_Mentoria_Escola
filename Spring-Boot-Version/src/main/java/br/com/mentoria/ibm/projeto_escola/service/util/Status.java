cb776f39ad7e7b2728b7a6fa699d9efb7843f33b
GabrielF-S <gabrielboni2008@hotmail.com>
2024-08-19 18:06:38 -0300

    Criado Enum para deificnir se o Aluno estava aprovado ou reprovado

---
+++ Spring-Boot-Version/src/main/java/br/com/mentoria/ibm/projeto_escola/service/util/Status.java
@@ -0,0 +1,8 @@
+package br.com.mentoria.ibm.projeto_escola.service.util;
+
+public enum Status {
+    APROVADO,
+    REPROVADO,
+    PENDENTE
+
+}
---
+++ notas-sender/src/main/java/br/com/mentoria/ibm/notas_sender/service/util/Status.java
@@ -0,0 +1,7 @@
+package br.com.mentoria.ibm.notas_sender.service.util;
+
+public enum Status {
+    APROVADO,
+    REPROVADO,
+    PENDENTE
+}
