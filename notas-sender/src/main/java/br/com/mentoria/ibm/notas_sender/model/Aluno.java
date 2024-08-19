b002332a57a2dd63d201355af8ac4d49ca633e39
GabrielF-S <gabrielboni2008@hotmail.com>
2024-08-19 18:03:02 -0300

    Adiconado anotação Setter do Lombok e atributos de email e status de aprovacao

--- notas-sender/src/main/java/br/com/mentoria/ibm/notas_sender/model/Aluno.java
+++ notas-sender/src/main/java/br/com/mentoria/ibm/notas_sender/model/Aluno.java
@@ -1,21 +1,23 @@
 package br.com.mentoria.ibm.notas_sender.model;
 
+import br.com.mentoria.ibm.notas_sender.service.util.Status;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
+@Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Aluno {
 
     private Integer id;
-
     private String primeiroNomeAluno;
-
     private String sobrenomeAluno;
-
     private int idade;
+    private Status status;
+    private  String email;
+
+
 
 }
