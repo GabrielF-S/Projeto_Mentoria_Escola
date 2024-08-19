b3ea1ab248f86453e138ac01b95633111626ed54
GabrielF-S <gabrielboni2008@hotmail.com>
2024-08-19 18:09:13 -0300

    AJustado script para inclusão de atributos no tb_aluno e difinicão correta de colunas

--- docker/scripts/init.sql
+++ docker/scripts/init.sql
@@ -1,30 +1,27 @@
 
 
  CREATE table tb_aluno(
-    id_aluno integer  serial,
+    id_aluno  serial PRIMARY KEY,
     nome_aluno varchar(50) not null,
     sobrenome_aluno varchar(100) not null,
     idade_aluno integer not null,
-    PRIMARY KEY(id_aluno)
+    email varchar(50)
+   aprovacao varchar(10),
 
 );
 
 CREATE table tb_turma(
-    id_turma integer  serial,
+    id_turma serial PRIMARY KEY,
     nome_turma varchar(50),
     id_aluno integer,
-    PRIMARY KEY(id_turma),
     FOREIGN KEY (id_aluno) REFERENCES tb_aluno (id_aluno)
 
 );
 
 CREATE table tb_escola(
-    id_escola  integer serial,
+    id_escola  serial PRIMARY KEY,
     nome_escola varchar(50),
     id_turma integer,
-
-    PRIMARY KEY(id_escola),
-    FOREIGN KEY (id_turma) REFERENCES tb_turma (id_turma),
+    FOREIGN KEY (id_turma) REFERENCES tb_turma (id_turma)
      
 );
-
